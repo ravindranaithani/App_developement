09f010bfde338af3e8d108ddde163fa940a7255a
ravindranaithani <92716697+ravindranaithani@users.noreply.github.com>
2023-12-08 06:31:57 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,4 +1,11 @@
-def largest_num(first,second,third):
-  list = [first,second,third]
+import streamlit as st
+st.title('Web App to find largest among three numbers')
+st.header('Enter your input here')
+first_number = st.number_input('Enter First Number')
+second_number = st.number_input('Enter Second Number')
+third_number = st.number_input('Enter Third Number')
+
+def find_largest_num(first_number,second_number,third_number):
+  list = [first_number,second_number,third_number]
   max = max(list)
   return max
